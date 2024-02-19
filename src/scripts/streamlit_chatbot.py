b6ba3cf3c3f423987d9531a35bcf3e29144229d4
miraculix95 <124256878+miraculix95@users.noreply.github.com>
2024-02-19 18:00:44 +0100

    Now with Classes used

--- src/scripts/streamlit_chatbot.py
+++ src/scripts/streamlit_chatbot.py
@@ -1,7 +1,6 @@
 # code from Streamlit itself - shows how to use ChatElements
 import streamlit as st
 import os
-from openai import OpenAI
 from chatbot import Chatbot
 from dotenv import load_dotenv, find_dotenv
 
@@ -9,10 +8,8 @@ load_dotenv(find_dotenv())
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 system_prompt = "You are Einstein's evil twin. You derive joy from claiming the opposite of what you know is true. You use flowery language and on occasion you let slip an exstatic expression of joy for fooling your audience."
+system_prompt = "Be nice to me"
 
-# chatbot = Chatbot(system_prompt=system_prompt)
-# chatbot.add_user_prompt("Why is the sky blue?")
-# response = chatbot.generate_response()
 
 with st.sidebar:
     if not openai_api_key:
@@ -29,7 +26,6 @@ st.title("💬 Chatbot")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": system_prompt},
-        {"role": "assistant", "content": "How can I help you?"},
     ]
 
 # Display chat history
@@ -42,23 +38,13 @@ if prompt := st.chat_input():
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    # _# replace
-    # chatbot = Chatbot(system_prompt=system_prompt) #problem: how do I add conversation history?
-    client = OpenAI(api_key=openai_api_key)
+    chatbot = Chatbot(system_prompt=system_prompt)
+    chatbot.conversation_history = st.session_state.messages
 
-    # _# replace
-    # Take out for version without a conversation history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
+    chatbot.add_user_prompt(prompt)
     st.chat_message("user").write(prompt)
 
-    # chatbot.add_user_prompt("Why is the sky blue?")
-    # response = chatbot.generate_response()
+    msg = chatbot.generate_response()
+    st.session_state.messages = chatbot.conversation_history
 
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo", messages=st.session_state.messages
-    )
-    msg = response.choices[0].message.content
-
-    st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
